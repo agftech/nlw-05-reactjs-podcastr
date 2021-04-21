79c849a1995fde085dca253e5a557a4d39ebf87c
agftech <agiraof@gmail.com>
2021-04-21 10:38:07 -0700

    :package: feat: add date formatting

--- src/component/Header/Index.tsx
+++ src/component/Header/Index.tsx
@@ -1,13 +1,18 @@
-import styles from './styles.module.scss';
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+
+import styles from "./styles.module.scss";
 
 export function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d, MMM", {
+    locale: ptBR,
+  });
+
   return (
     <header className={styles.headerContainer}>
       <img src="/logo.svg" alt="Podcastr" />
-
       <p>o melhor para você ouvir, sempre</p>
-
-      <span>Ter, 21 abril</span>
+      <span>{currentDate}</span>
     </header>
   );
 }
