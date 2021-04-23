2fe3a7e23d8ec08fde2fdb0dd3c8832e6023723d
agftech <agiraof@gmail.com>
2021-04-23 11:34:02 -0700

    :package: feat: add listing of the last two podcasts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { api } from "../services/api";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
@@ -16,15 +18,43 @@ type Episode = {
 };
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Array<Episode>;
 };
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes }: HomeProps) {
   return (
-    <>
-      <h1>index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </>
+    <div>
+      <section>
+        <h2>Últimos Lançamentos</h2>
+        <ul>
+          {latestEpisodes.map((episode) => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
   );
 }
 
@@ -54,9 +84,11 @@ export const getStaticProps: GetStaticProps = async () => {
     };
   });
 
+  const latestEpisodes = episodes.slice(0, 2);
+
   return {
     props: {
-      episodes,
+      latestEpisodes,
     },
     revalidate: 60 * 60 * 2,
   };
