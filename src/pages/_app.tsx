c98953f1766fe2c9790b4acacef69bc133c9cc1f
agftech <agiraof@gmail.com>
2021-04-24 09:46:38 -0700

    :package: feat: add context to the music player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,19 @@ import styles from "../styles/app.module.scss";
 
 import { Header } from "../component/Header/Index";
 import { Player } from "../component/Player/Index";
+import { PlayerContext } from "../contexts/PlayerContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={"TEST"}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
