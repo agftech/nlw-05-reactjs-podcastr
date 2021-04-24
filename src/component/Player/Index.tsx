f601350eeae3cb067e388552d100b2d7ce5f65f2
agftech <agiraof@gmail.com>
2021-04-24 10:47:04 -0700

    :package: feat: add podcast information to the music player

--- src/component/Player/Index.tsx
+++ src/component/Player/Index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
 
@@ -12,12 +13,25 @@ export function Player() {
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Tocando agora" />
-        <strong>Tocando agora {episode?.title}</strong>
+        <strong>Tocando agora</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
       <footer className={styles.empty}>
         <div className={styles.progress}>
