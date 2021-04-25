71a63b369838fac52e4523de91bc051ae1d9e03b
agftech <agiraof@gmail.com>
2021-04-25 11:51:34 -0700

    :recycle: refactor: improve music player context

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 type Episode = {
   title: string;
@@ -17,4 +17,45 @@ type PlayerContextData = {
   setPlayingState: (state: boolean) => void;
 };
 
+type PlayerContextProviderProps = {
+  children: ReactNode;
+};
+
 export const PlayerContext = createContext({} as PlayerContextData);
+
+export function PlayerContextProvider({
+  children,
+}: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
